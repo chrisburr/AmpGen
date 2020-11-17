11754081f496be0b114b2bd05e4c9bd02072e32c
Travis CI <travis@travis-ci.org>
2020-11-17 12:19:57 +0000

    Deploy code docs to GitHub Pages Travis build: 389
    Commit: ea309906990d72006d3dc6c797c373482cf8b6e4

--- navtreedata.js
+++ navtreedata.js
@@ -1,27 +1,3 @@
-/*
- @licstart  The following is the entire license notice for the JavaScript code in this file.
-
- The MIT License (MIT)
-
- Copyright (C) 1997-2020 by Dimitri van Heesch
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- and associated documentation files (the "Software"), to deal in the Software without restriction,
- including without limitation the rights to use, copy, modify, merge, publish, distribute,
- sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all copies or
- substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
- BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
- @licend  The above is the entire license notice for the JavaScript code in this file
-*/
 var NAVTREE =
 [
   [ "AmpGen", "index.html", [
@@ -50,6 +26,7 @@ var NAVTREE =
     [ "File List", "files.html", "files" ],
     [ "File Members", "globals.html", [
       [ "All", "globals.html", null ],
+      [ "Functions", "globals_func.html", null ],
       [ "Macros", "globals_defs.html", null ]
     ] ]
   ] ]
@@ -58,19 +35,19 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "annotated.html",
-"d1/d00/struct_amp_gen_1_1_a_v_x512d_1_1complex__v.html#a1fbd88a861fd58c083126a7165b5a3b7",
-"d2/dee/struct_amp_gen_1_1_keyed_functors_3_01return__type_07arg__types_8_8_8_08_4.html",
-"d4/d0e/class_amp_gen_1_1_tensor.html#a96aae33aadb91a71895873ade3c4c686",
-"d5/d9b/struct_amp_gen_1_1_function.html#a280dc508e7b6e99c49c629085aaa1b14",
-"d6/d9f/avx2f__types_8h.html",
-"d8/d3c/class_amp_gen_1_1_spline.html#a426bc8e0d7d3673f10e29cdb8ffcbe27",
-"d9/d37/struct_amp_gen_1_1_a_v_x2d_1_1complex__v.html#a7a0cadca610d3cc8e86fbb1de4558dc4",
-"da/dab/class_amp_gen_1_1_sub.html",
-"db/d51/class_amp_gen_1_1_greater_than.html",
-"dc/d82/namespace_amp_gen.html#d4/d7d/struct_amp_gen_1_1_l_s",
-"de/d21/class_amp_gen_1_1_quark_content.html#ae43c90dc30b47bb51392f03aab04d811",
-"df/d3d/_event_list_8h.html#a958229e0aff4893b1e65bcb6eb3c83fb",
-"namespacemembers_n.html"
+"d1/d10/class_amp_gen_1_1t_coherent_sum.html#aa1b83663da7823cbf5b6cf1aa922b8ca",
+"d2/d28/class_amp_gen_1_1_tan.html",
+"d3/dae/class_amp_gen_1_1p_coherent_sum2.html#aba3ea98eb93914e4da266aeba67ca6c5",
+"d4/d48/class_amp_gen_1_1p_coherent_sum.html#aa3792ddf012f77327caec6677ebe7a7e",
+"d5/dfb/class_amp_gen_1_1_integrator.html#add7a16b0d9757838c20552c867d4ae0b",
+"d7/d5c/struct_amp_gen_1_1_vertex_1_1f___vf___s_r.html#adc2dcce8383aaa05460aba7d9e198a57",
+"d9/d03/class_amp_gen_1_1_lineshape_1_1_f_o_c_u_s.html#a5c76ad355f3cf89aca1852b8a7ed5d13",
+"da/d76/class_amp_gen_1_1_log.html#a3c8369ae0fe3f7ff6091595c02fe0e9e",
+"db/d37/class_amp_gen_1_1_particle_properties.html#af53d33fad2d432208164c0afaaee9188",
+"dc/d1e/class_amp_gen_1_1_exp.html#a9691967573670594c693eb3506b9b237",
+"dd/dad/class_amp_gen_1_1_lineshape_1_1_l_a_s_s.html#ad976467da5d6af6ce69ea73de955aed8",
+"de/dd7/class_amp_gen_1_1_particle.html#aa3b7bb85a641f2907497b1f71f58b78d",
+"functions_vars_f.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
