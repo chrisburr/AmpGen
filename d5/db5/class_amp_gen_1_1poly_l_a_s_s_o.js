6ebc44d1605b20ecd249a93dca54b3a0b1b8db1f
Travis CI <travis@travis-ci.org>
2021-02-12 07:38:20 +0000

    Deploy code docs to GitHub Pages Travis build: 407
    Commit: cb7552a947164fcea06be293743b661abc1fd1bb

--- d5/db5/class_amp_gen_1_1poly_l_a_s_s_o.js
+++
@@ -1,6 +0,0 @@
-var class_amp_gen_1_1poly_l_a_s_s_o =
-[
-    [ "polyLASSO", "d5/db5/class_amp_gen_1_1poly_l_a_s_s_o.html#af77eca63f3acb42a9270537939688e67", null ],
-    [ "getVal", "d5/db5/class_amp_gen_1_1poly_l_a_s_s_o.html#ae9f60fb79572885d6d3c7e1f93e5db16", null ],
-    [ "penalty", "d5/db5/class_amp_gen_1_1poly_l_a_s_s_o.html#a262b88d2db6ccc27974a36f54d46daef", null ]
-];
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['gausserrorpropagator',['GaussErrorPropagator',['../d1/de8/class_amp_gen_1_1_gauss_error_propagator.html',1,'AmpGen']]],
-  ['gaussian',['Gaussian',['../d0/d81/class_amp_gen_1_1_lineshape_1_1_gaussian.html',1,'AmpGen::Lineshape']]],
-  ['gaussianconstraint',['GaussianConstraint',['../d7/deb/class_amp_gen_1_1_gaussian_constraint.html',1,'AmpGen']]],
-  ['gcoherentsum',['gCoherentSum',['../da/dbf/class_amp_gen_1_1g_coherent_sum.html',1,'AmpGen']]],
-  ['generator',['Generator',['../de/d94/class_amp_gen_1_1_generator.html',1,'AmpGen']]],
-  ['generickmatrix',['GenericKmatrix',['../d9/d67/class_amp_gen_1_1_lineshape_1_1_generic_kmatrix.html',1,'AmpGen::Lineshape']]],
-  ['glass',['GLASS',['../dd/d6a/class_amp_gen_1_1_lineshape_1_1_g_l_a_s_s.html',1,'AmpGen::Lineshape']]],
-  ['gounarissakurai',['GounarisSakurai',['../d3/dbe/class_amp_gen_1_1_lineshape_1_1_gounaris_sakurai.html',1,'AmpGen::Lineshape']]],
-  ['greaterthan',['GreaterThan',['../db/d51/class_amp_gen_1_1_greater_than.html',1,'AmpGen']]],
-  ['gspline',['GSpline',['../dd/d41/class_amp_gen_1_1_lineshape_1_1_g_spline.html',1,'AmpGen::Lineshape']]]
+  ['gausserrorpropagator_1314',['GaussErrorPropagator',['../d1/de8/class_amp_gen_1_1_gauss_error_propagator.html',1,'AmpGen']]],
+  ['gaussian_1315',['Gaussian',['../d0/d81/class_amp_gen_1_1_lineshape_1_1_gaussian.html',1,'AmpGen::Lineshape']]],
+  ['gaussianconstraint_1316',['GaussianConstraint',['../d7/deb/class_amp_gen_1_1_gaussian_constraint.html',1,'AmpGen']]],
+  ['generator_1317',['Generator',['../de/d94/class_amp_gen_1_1_generator.html',1,'AmpGen']]],
+  ['generickmatrix_1318',['GenericKmatrix',['../d9/d67/class_amp_gen_1_1_lineshape_1_1_generic_kmatrix.html',1,'AmpGen::Lineshape']]],
+  ['glass_1319',['GLASS',['../dd/d6a/class_amp_gen_1_1_lineshape_1_1_g_l_a_s_s.html',1,'AmpGen::Lineshape']]],
+  ['gounarissakurai_1320',['GounarisSakurai',['../d3/dbe/class_amp_gen_1_1_lineshape_1_1_gounaris_sakurai.html',1,'AmpGen::Lineshape']]],
+  ['greaterthan_1321',['GreaterThan',['../db/d51/class_amp_gen_1_1_greater_than.html',1,'AmpGen']]],
+  ['greaterthanequalto_1322',['GreaterThanEqualTo',['../d3/def/class_amp_gen_1_1_greater_than_equal_to.html',1,'AmpGen']]],
+  ['gspline_1323',['GSpline',['../dd/d41/class_amp_gen_1_1_lineshape_1_1_g_spline.html',1,'AmpGen::Lineshape']]]
 ];
